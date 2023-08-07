28501687e1e2357c7f2f6a22665fcf0c266e5444
Nitish Kumar <nk9990207902@gmail.com>
2023-08-07 19:24:55 +0530

    7/8/23

---
+++ routers/Auth.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/UserSchema.js");
+const bcrypt = require("bcryptjs");
+
+// Home
+router.get("/", (req, res) => {
+  res.send("Home From Server");
+});
+
+// Register
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).send("Filled all the Details carefully");
+  }
+
+  const alreadyUser = await User.findOne({ email });
+
+  if (alreadyUser) {
+    return res.status(422).send({ message: "Already Registered" });
+  }
+
+  const salt = await bcrypt.genSaltSync(12);
+  const hashedPassword = await bcrypt.hashSync(password, salt);
+
+  const user = await new User({ name, email, password: hashedPassword });
+  await user.save();
+
+  return res.send({ message: "Registered Successfully" });
+});
+
+// Login
+
+
+module.exports = router;
